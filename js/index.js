62db8e0caad8873e6f22114049ec866f7562e76a
rayan nahi <rayan.nahi99@gmail.com>
2021-06-04 10:51:32 +0200

    fin de l'index.html et index.js

---
+++ js/index.js
@@ -0,0 +1,32 @@
+async function init() {
+  const teddies = await getTeddies();
+  coverPage(teddies);
+}
+init();
+async function getTeddies (){
+  return fetch("http://localhost:3000/api/teddies")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((teddies)=>teddies)
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
+}
+function coverPage(teddies) {
+  teddies.forEach((teddy) => {
+    showTeddy(teddy);
+  })
+}
+  function showTeddy(teddy) {
+    const elt = document.getElementById('covermodel');
+    const dupNode = document.importNode(elt.content,true);
+    dupNode.getElementById('imgTeddy').src= teddy.imageUrl;
+    dupNode.getElementById("nameTeddy").textContent= teddy.name;
+    dupNode.getElementById("priceTeddy").textContent= teddy.price/100+"€";
+    dupNode.getElementById("descriptionTeddy").textContent= teddy.description;
+    dupNode.getElementById("btnTeddy").href= "products.html?id="+teddy._id;
+    document.getElementById("teddies").appendChild(dupNode);
+  }
